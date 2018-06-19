2c471c7478b3404f3ba32f3477df05b6c641e2c7
DominicAbogado <dominic.j.abogado@gmail.com>
2018-06-19 12:03:38 -0400

    figuring out spotify

--- OMDBTest.js
+++ OMDBTest.js
@@ -6,6 +6,7 @@ var args = process.argv.slice(2)
   
   // Then run a request to the OMDB API with the movie specified
   //OMDB Functionality
+  askMovie()
 function askMovie(){
 inquirer
   .prompt([
--- testSpotify.js
+++ testSpotify.js
@@ -1,8 +1,18 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 var twitter = require("twitter");
-var SpotifyWebApi = require("spotify-web-api-node");
+var Spotify = require("node-spotify-api");
 
-var spotify = new SpotifyWebApi(keys.spotify);
+var spotify = new Spotify(keys.spotify);
 
+spotify
+  .search({ type: 'track', query: 'Heatstroke', limit: 1 })
+  .then(function(response) {
+    console.log("Artists: " + response.tracks.items[0].artists[0].name);
+    console.log("------------------------------------------");
+    console.log("Track Name: " + response.tracks.items[0].name);
 
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
