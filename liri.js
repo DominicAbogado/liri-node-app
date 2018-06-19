81a0e6a152f979dd3ba2ad66991be98f12c3903a
DominicAbogado <dominic.j.abogado@gmail.com>
2018-06-19 12:39:31 -0400

    All the functions are put together

--- liri.js
+++ liri.js
@@ -1,22 +1,14 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 var twitter = require("twitter");
-var SpotifyWebApi = require("spotify-web-api-node");
+var Spotify = require("node-spotify-api");
 var request = require("request");
 
 var inquirer = require("inquirer");
 
-var spotify = new SpotifyWebApi(keys.spotify);
+var spotify = new Spotify(keys.spotify);
 var client = new twitter(keys.twitter);
 
-spotify.getArtistAlbums("Taylor Swift").then(
-  function(data) {
-    console.log("Artist albums", data.body);
-  },
-  function(err) {
-    console.error(err);
-  }
-);
 
 var myTweets = "my-tweets";
 var mySong = "spotify-this-song";
@@ -37,6 +29,40 @@ function postTweet() {
     }
   });
 }
+
+// Spotify Function
+function askSong(){
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message:
+          "Please type in the name of a song",
+        name: "songTitle",
+      }
+    ])
+    .then(function(ir) {
+spotify
+.search({ type: 'track', query: ir.songTitle, limit: 1 })
+.then(function(response) {
+  console.log("Artists: " + response.tracks.items[0].artists[0].name);
+  console.log("------------------------------------------");
+  console.log("Track Name: " + response.tracks.items[0].name);
+  console.log("------------------------------------------");
+  if (response.tracks.items[0].preview_url == null){
+      console.log("No Preview for this song... Guess you have to listen on youtube!")
+  } else{
+  console.log("Listen to a preview here: " + response.tracks.items[0].preview_url);
+  }
+  console.log("------------------------------------------");
+  console.log("Album: "+ response.tracks.items[0].album.name);
+
+})
+.catch(function(err) {
+  console.log('Error Occurred: ' + err);
+});
+})
+};
 
 //OMDB Function
 function askMovie(){
@@ -94,31 +120,25 @@ inquirer
     {
       type: "confirm",
       message:
-        "Welcome to LIRI! \n I can perform a number of functions for you \n please confirm to continue!",
+        "Welcome to LIRI! \n I can perform a number of functions for you! \n Would you like to continue!?",
       name: "opening"
     },
     {
       type: "list",
       message: "Please choose one of the following commands:",
-      choices: ["my-Tweets","my-Movies" , "end game"],
+      choices: ["My-Tweets","My-Movies", "My-Songs", "end game"],
       name: "action"
     }
   ])
   .then(function(ir) {
-    if (ir.action === "my-Tweets") {
+    if (ir.action === "My-Tweets") {
       console.log("Here for your most recent tweets!");
       postTweet();
-    } else if (ir.action === "my-Movies") {
-      askMovie()
+    } else if (ir.action === "My-Movies") {
+      askMovie();
+    } else if (ir.action === "My-Songs") {
+      askSong();
     } else {
       console.log("Thank you for playing!");
     }
   });
-
-if (process.argv[2] == myTweets) {
-  postTweet();
-} else {
-  console.log("What would you like me to do?");
-}
-
-// Spotify API
--- testSpotify.js
+++ testSpotify.js
@@ -2,17 +2,40 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var twitter = require("twitter");
 var Spotify = require("node-spotify-api");
+var inquirer = require("inquirer");
 
 var spotify = new Spotify(keys.spotify);
 
+askSong();
+function askSong(){
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message:
+            "Please type in the name of a song",
+          name: "songTitle",
+        }
+      ])
+      .then(function(ir) {
 spotify
-  .search({ type: 'track', query: 'Heatstroke', limit: 1 })
+  .search({ type: 'track', query: ir.songTitle, limit: 1 })
   .then(function(response) {
     console.log("Artists: " + response.tracks.items[0].artists[0].name);
     console.log("------------------------------------------");
     console.log("Track Name: " + response.tracks.items[0].name);
+    console.log("------------------------------------------");
+    if (response.tracks.items[0].preview_url == null){
+        console.log("No Preview for this song... Guess you have to listen on youtube!")
+    } else{
+    console.log("Listen to a preview here: " + response.tracks.items[0].preview_url);
+    }
+    console.log("------------------------------------------");
+    console.log("Album: "+ response.tracks.items[0].album.name);
 
   })
   .catch(function(err) {
     console.log(err);
   });
+})
+};
