51f4bbb39144a216e0e1a817cbf76c5c895751c2
DominicAbogado <dominic.j.abogado@gmail.com>
2018-06-18 20:55:29 -0400

    Did OMDB

---
+++ OMDBTest.js
@@ -0,0 +1,102 @@
+// Include the request npm package (Don't forget to run "npm install request" in this folder first!)
+var request = require("request");
+var inquirer = require("inquirer")
+
+var args = process.argv.slice(2)
+  
+  // Then run a request to the OMDB API with the movie specified
+  //OMDB Functionality
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message:
+        "Please type in the name of a movie",
+      name: "movieTitle",
+    }
+  ])
+  .then(function(ir) {
+    request(
+      "http://www.omdbapi.com/?t=" +
+        ir.movieTitle +
+        "&y=&plot=short&tomatoes=true&apikey=trilogy",
+      function(error, response, body) {
+        // If the request is successful (i.e. if the response status code is 200)
+        if (!error && response.statusCode === 200) {
+          
+          var mv = JSON.parse(body);
+          // Parse the body of the site and recover just the imdbRating
+          // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+          console.log("Movie Title: " + mv.Title);
+          console.log("------------------------------------------");
+          console.log("Released: " + mv.Released);
+          console.log("------------------------------------------");
+          console.log("IMDB Rating: " + mv.imdbRating);
+          console.log("------------------------------------------");
+          ;
+          if(mv.tomatoMeter == "N/A"){
+            console.log("Rotten Tomatoes Score Not Available For This Title")
+          } else {
+            console.log("Rotten Tomatoes Score: " + mv.tomatoMeter);
+          }
+          console.log("------------------------------------------");
+          console.log("Produced in: " + mv.Country);
+          console.log("------------------------------------------");
+          console.log("Language: " + mv.Language);
+          console.log("------------------------------------------");
+          console.log("Plot: " + mv.Plot);
+          console.log("------------------------------------------");
+          console.log("Starring: " + mv.Actors);
+          console.log("------------------------------------------");
+        }
+      }
+    )
+  });
+  
+
+// ])
+// .then(function(ir) {
+//   inquirer.prompt([{
+//     type: "confirm",
+//     message: "The movie you typed was: " + ir.movieTitle + "\n Is this correct?",
+//     name: "movieConfirm",
+//     default: true
+//   }
+
+// getMovie() 
+// function getMovie() {
+//   request(
+//     "http://www.omdbapi.com/?t=" +
+//       args +
+//       "&y=&plot=short&tomatoes=true&apikey=trilogy",
+//     function(error, response, body) {
+//       // If the request is successful (i.e. if the response status code is 200)
+//       if (!error && response.statusCode === 200) {
+//         var mv = JSON.parse(body);
+//         // Parse the body of the site and recover just the imdbRating
+//         // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+//         console.log("Movie Title: " + mv.Title);
+//         console.log("------------------------------------------");
+//         console.log("Released: " + mv.Released);
+//         console.log("------------------------------------------");
+//         console.log("IMDB Rating: " + mv.imdbRating);
+//         console.log("------------------------------------------");
+//         ;
+//         if(mv.tomatoMeter == "N/A"){
+//           console.log("Rotten Tomatoes Score Not Available For This Title")
+//         } else {
+//           console.log("Rotten Tomatoes Score: " + mv.tomatoMeter);
+//         }
+//         console.log("------------------------------------------");
+//         console.log("Produced in: " + mv.Country);
+//         console.log("------------------------------------------");
+//         console.log("Language: " + mv.Language);
+//         console.log("------------------------------------------");
+//         console.log("Plot: " + mv.Plot);
+//         console.log("------------------------------------------");
+//         console.log("Starring: " + mv.Actors);
+//         console.log("------------------------------------------");
+//       }
+//     }
+//   );
--- liri.js
+++ liri.js
@@ -2,15 +2,16 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var twitter = require("twitter");
 var SpotifyWebApi = require("spotify-web-api-node");
+var request = require("request");
 
 var inquirer = require("inquirer");
 
 var spotify = new SpotifyWebApi(keys.spotify);
 var client = new twitter(keys.twitter);
 
-spotify.getArtistAlbums('Taylor Swift').then(
+spotify.getArtistAlbums("Taylor Swift").then(
   function(data) {
-    console.log('Artist albums', data.body);
+    console.log("Artist albums", data.body);
   },
   function(err) {
     console.error(err);
@@ -22,50 +23,86 @@ var mySong = "spotify-this-song";
 var myMovie = "movie-this";
 var myDo = "do-what-it-says";
 
-// //Twitter Function
-// function postTweet() {
-//   client.get("statuses/user_timeline", function(error, tweets, response) {
-//     if (error) {
-//       console.log("Wrong");
-//     }
-//     for (var i = 0; i < tweets.length; i++) {
-//       console.log("------------------------------------------");
-//       console.log("Created at: " + tweets[i].created_at);
-//       console.log("text: " + tweets[i].text);
-//       console.log("------------------------------------------");
-//     }
-//   });
-// }
+//Twitter Function
+function postTweet() {
+  client.get("statuses/user_timeline", function(error, tweets, response) {
+    if (error) {
+      console.log("Wrong");
+    }
+    for (var i = 0; i < tweets.length; i++) {
+      console.log("------------------------------------------");
+      console.log("Created at: " + tweets[i].created_at);
+      console.log("text: " + tweets[i].text);
+      console.log("------------------------------------------");
+    }
+  });
+}
 
+//OMDB Function
+function getMovie() {
+  request(
+    "http://www.omdbapi.com/?t=" +
+      movieTitle +
+      "&y=&plot=short&tomatoes=true&apikey=trilogy",
+    function(error, response, body) {
+      // If the request is successful (i.e. if the response status code is 200)
+      if (!error && response.statusCode === 200) {
+        var mv = JSON.parse(body);
+        console.log("Movie Title: " + mv.Title);
+        console.log("------------------------------------------");
+        console.log("Released: " + mv.Released);
+        console.log("------------------------------------------");
+        console.log("IMDB Rating: " + mv.imdbRating);
+        console.log("------------------------------------------");
+        if (mv.tomatoMeter == "N/A") {
+          console.log("Rotten Tomatoes Score Not Available For This Title");
+        } else {
+          console.log("Rotten Tomatoes Score: " + mv.tomatoMeter);
+        }
+        console.log("------------------------------------------");
+        console.log("Produced in: " + mv.Country);
+        console.log("------------------------------------------");
+        console.log("Language: " + mv.Language);
+        console.log("------------------------------------------");
+        console.log("Plot: " + mv.Plot);
+        console.log("------------------------------------------");
+        console.log("Starring: " + mv.Actors);
+        console.log("------------------------------------------");
+      }
+    }
+  );
+}
 
+//LIRI Inquire Function
+inquirer
+  .prompt([
+    {
+      type: "confirm",
+      message:
+        "Welcome to LIRI! \n I can perform a number of functions for you \n please confirm to continue!",
+      name: "opening"
+    },
+    {
+      type: "list",
+      message: "Please choose one of the following commands:",
+      choices: [myTweets, myMovie, "end game"],
+      name: "action"
+    }
+  ])
+  .then(function(ir) {
+    if (ir.action === MyTweets) {
+      console.log("Here for your most recent tweets!");
+      postTweet();
+    } else if (ir.action === myMovie) {
+    } else {
+      console.log("Thank you for playing!");
+    }
+  });
 
-// //LIRI Inquire Function
-// inquirer.prompt([
-//   {
-//     type: "confirm",
-//     message: "Welcome to LIRI! \n I can perform a number of functions for you \n please confirm to continue!",
-//     name: "opening",
-//   },
-//   {
-//     type: "list",
-//     message: "Please choose one of the following commands:",
-//     choices: ["my-tweets", "end game"],
-//     name: "action"
-//   }
-// ]).then(function(ir){
-//   if(ir.action === "my-tweets"){
-//     console.log("Here for your most recent tweets!");
-//     postTweet();
-//   } else {
-//     console.log("Thank you for playing!")
-//   }
-// });
-
-// if (process.argv[2] == myTweets) {
-//   postTweet();
-// } else {
-//   console.log("What would you like me to do?");
-// }
-
+if (process.argv[2] == myTweets) {
+  postTweet();
+} else {
+  console.log("What would you like me to do?");
+}
 
 // Spotify API
